13575d5711826ec48be6ecca317f6244258ca54c
Yuls <152199674+jsanjosel01@users.noreply.github.com>
2026-05-22 12:29:40 +0200

    ❌Borrar otros proyectos.

--- reactRouter/src/interfaces/Product.ts
+++
@@ -1,6 +0,0 @@
-export interface Product{
-    id: string
-    nombre: string
-    precio: number
-    descripcion?: string
-}
--- reactRouter/src/interfaces/SessionUser.ts
+++
@@ -1,7 +0,0 @@
-import type { User } from '@supabase/supabase-js'
-import type { Profile } from './Profile'
-
-export interface SessionUser {
-    user: User
-    profile: Profile | null
-}
